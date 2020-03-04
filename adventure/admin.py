308385bed5a25d836af6635c449321d3684134ca
Reese Kunz <reesekunz21@gmail.com>
2020-03-03 23:33:19 -0700

    set up super user for admin account and registered models

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Room, Player
+
+# From tutorial - can ignore for now
+# class NoteAdmin(admin.ModelAdmin):
+#     # read only fields from models.py
+#     readonly_fields = ("created_at", "last_modified")
+
+
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Player)
