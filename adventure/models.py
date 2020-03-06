e04364b567249fd62a3cd0e93f4ea18eaf5a585c
Cory Hale <corylhale2013@gmail.com>
2020-03-05 22:20:22 -0700

    made change in models.py

--- adventure/models.py
+++ adventure/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-import uuid
 from uuid import uuid4
 
 
